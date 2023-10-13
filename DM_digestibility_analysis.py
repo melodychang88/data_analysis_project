e3f70ede27d575d75d4a16ce4134deabc18cb4f1
Melody Chang <contact.melodychang@gmail.com>
2023-10-13 16:47:06 +0800

    bulit function

--- DM_digestibility_analysis.py
+++ DM_digestibility_analysis.py
@@ -1,75 +1,120 @@
 #read csv
 import csv
-all_rows = []
-with open("sow1_CON_DM_digestibility.csv", encoding='utf-8-sig') as csvfile :
-    raw_data = csv.reader(csvfile)
-    #預設以列讀取data
-    for row in raw_data:
-        #把數據加進去空list裡
-        all_rows.append(row)
 
-
-# transform list to dictionary
-#第0列為各column名稱 
-col_names = all_rows[0]
-all_data = []
-#第一列以下的data為dict的value
-for row in all_rows[1:]:
-    #一個key對一個value
-    i = 0
-    row_dict = {}
-    for col_name in col_names:
-        row_dict[col_name] = row[i]
-        i += 1
-    all_data.append(row_dict)
-           
-all_data
-
-#calculate diet dry matter
-diet_dry_matter = []
-#從all_data中一次取一個dict
-for data in all_data:
-    dm_dict = {}
-    dm_dict['Diet_DM'] = round(float(data['Diet_weight'])*float(data['Diet_DM_percentage']), 4)
-    diet_dry_matter.append(dm_dict)
-
-diet_dry_matter
-
-#calculate digesta weight
-digesta_weight = []
-#從all_data中一次取一個dict
-for data in all_data:
-    digesta_dict = {}
-    #round(X變數, n取幾位)
-    digesta_dict['Digesta_weight'] = round(float(data['Freeze_dry_weight'])-float(data['Tube_weight']), 4)
-    digesta_weight.append(digesta_dict)
-
-digesta_weight
-
-#calculate DM digestibility
-dry_matter_digestibility = []
-for diet_data, digesta_data in zip(diet_dry_matter, digesta_weight):
-    DM_digestibility_dict = {}
-    diet_DM = float(diet_data['Diet_DM'])
-    digesta_wt = float(digesta_data['Digesta_weight'])
-    DM_digestibility_dict['DM_digestibility'] = round((diet_DM - digesta_wt) / diet_DM * 100, 4)
-    dry_matter_digestibility.append(DM_digestibility_dict)
+# fuction for reading csv
+def read_csv(filename):
+    all_rows = []
     
-dry_matter_digestibility
+    # use encoding "utf-8-sig" read csv
+    with open(filename, encoding='utf-8-sig') as csvfile:
+        raw_data = csv.reader(csvfile)
+        # read raw data in row
+        for row in raw_data:
+            # data is added into list "all_rows"
+            all_rows.append(row)
+    
+    return all_rows
 
-# put the digestibility in sequence
-digestibility_sequence = []
-for digest_data in dry_matter_digestibility:
-    digestibility_sequence.append(digest_data['DM_digestibility'])
+treatment1_raw= read_csv("sow1_CON_DM_digestibility.csv")
+treatment2_raw= read_csv("sow1_0.1%_DM_digestibility.csv")
+treatment3_raw= read_csv("sow1_0.2%_DM_digestibility.csv")
 
-digestibility_sequence.sort()
-print(digestibility_sequence)
 
-# delete extreme value
-def filter_extremes(data, lower_threshold, upper_threshold):
-    filtered_data = [x for x in data if lower_threshold <= x <= upper_threshold]
-    return filtered_data
- 
+# function for transforming list to dictionary
+def raw_data_to_dict(treatment):
+    #第0列為各column名稱 
+    col_names = treatment[0]
+    all_data = []
+    
+    #第一列以下的data為dict的value
+    for row in treatment[1:]:
+        #一個key對一個value
+        i = 0
+        row_dict = {}
+        for col_name in col_names:
+            row_dict[col_name] = row[i]
+            i += 1
+        all_data.append(row_dict)
+    
+    return all_data
+
+treatment1_dict= raw_data_to_dict(treatment1_raw)       
+treatment2_dict=raw_data_to_dict(treatment2_raw)
+treatment3_dict=raw_data_to_dict(treatment3_raw)
+
+# function for multipling data 
+def multiply_data(treatment_dict, new_key, key1, key2):
+    data_multiplication = []
+    # 從treatment_dict中一次取一個dict處理
+    for data in treatment_dict:
+        multiplication_dict = {}
+        # built a new_key in multiplication_dict, value is key1*key2
+        # the value of key1 and key2 is str, which needs to change to float
+        #round(variable, n取幾位)
+        multiplication_dict[new_key] = round(float(data[key1])*float(data[key2]), 4)
+        # dict is added into list "data_multiplication"
+        data_multiplication.append(multiplication_dict)
+
+    return data_multiplication
+
+# use "multiply_data" function to gain diet dry matter 
+treatment1_diet_DM= multiply_data(treatment1_dict, 'Diet_DM', 'Diet_weight', 'Diet_DM_percentage')
+treatment2_diet_DM= multiply_data(treatment2_dict, 'Diet_DM', 'Diet_weight', 'Diet_DM_percentage')
+treatment3_diet_DM= multiply_data(treatment3_dict, 'Diet_DM', 'Diet_weight', 'Diet_DM_percentage')
+
+
+# function for subtrcting data
+def subtract_data(treatment_dict, new_key, key1, key2):
+    data_subtraction = []
+    # 從treatment_dict中一次取一個dict處理
+    for data in treatment_dict:
+        subtraction_dict = {}
+        # built a new_key in subtraction_dict, value is key1-key2
+        # the value of key1 and key2 is str, which needs to change to float
+        #round(variable, n取幾位)
+        subtraction_dict[new_key] = round(float(data[key1])-float(data[key2]), 4)
+        # dict is added into list "data_subtraction"
+        data_subtraction.append(subtraction_dict)
+    
+    return data_subtraction
+
+# use "substract_data" function to gain digesta weight
+treatment1_digesta_weight= subtract_data(treatment1_dict, 'Digesta_weight', 'Freeze_dry_weight', 'Tube_weight')
+treatment2_digesta_weight= subtract_data(treatment2_dict, 'Digesta_weight', 'Freeze_dry_weight', 'Tube_weight')
+treatment3_digesta_weight= subtract_data(treatment3_dict, 'Digesta_weight', 'Freeze_dry_weight', 'Tube_weight')
+
+
+# calculate DM digestibility
+def calculate_DM_digestibility(treatment_diet_DM, treatment_digesta_weight):
+    dry_matter_digestibility = []
+    for diet_data, digesta_data in zip(treatment_diet_DM, treatment_digesta_weight):
+        DM_digestibility_dict = {}
+        diet_DM = float(diet_data['Diet_DM'])
+        digesta_wt = float(digesta_data['Digesta_weight'])
+        DM_digestibility_dict['DM_digestibility'] = round((diet_DM - digesta_wt) / diet_DM * 100, 4)
+        dry_matter_digestibility.append(DM_digestibility_dict)
+    
+    return dry_matter_digestibility
+
+# use "calculate_DM_digestibility" function to gain DM digestibility
+treatment1_DM_digestibility= calculate_DM_digestibility(treatment1_diet_DM, treatment1_digesta_weight)
+treatment2_DM_digestibility= calculate_DM_digestibility(treatment2_diet_DM, treatment2_digesta_weight)
+treatment3_DM_digestibility= calculate_DM_digestibility(treatment3_diet_DM, treatment3_digesta_weight)
+
+# sort data from the smallest to the largest
+def sort_data(treatment_data, data_key):
+    data_sequence= []
+    for data in treatment_data:
+        data_sequence.append(float(data[data_key]))
+    
+    data_sequence.sort()
+    return data_sequence
+
+# use "sort_data" function to sort data
+treatment1_DM_digestibility_sequence= sort_data(treatment1_DM_digestibility, 'DM_digestibility')
+treatment2_DM_digestibility_sequence= sort_data(treatment2_DM_digestibility, 'DM_digestibility')
+treatment3_DM_digestibility_sequence= sort_data(treatment3_DM_digestibility, 'DM_digestibility')
+
  
  
  

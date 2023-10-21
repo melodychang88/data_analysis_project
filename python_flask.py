b5e84780a6ceebb4c8105c93d54b00446fcce02c
Melody Chang <contact.melodychang@gmail.com>
2023-10-21 22:24:31 +0800

    write code to upload csv file

--- python_flask.py
+++ python_flask.py
@@ -1,39 +1,29 @@
-# import Flask
-from flask import Flask
-# import request object
-from flask import request
-from flask import redirect #載入redirect函式
-from flask import render_template #載入render_template函式
-import json #載入json模組
+# import Flask, request, render_template
+from flask import Flask, request, render_template
+import os
 
-# bulit application object ,可以設定靜態檔案的路徑
-app=Flask(__name__, static_folder="static",   #靜態檔案的資料夾名稱
-    static_url_path="/"    #靜態檔案對應的網址名稱 http://127.0.0.1:3000/mike.jpg 
-    )
-#所有在static 資料夾底下的檔案，都對應到網址路徑 /檔案名稱
+# bulit application object
+app=Flask(__name__)
+
 
 # 建立路徑 / 對應的處理函式
 # /代表網站首頁
 #使用GET來方法，處理路徑 / 的處理函式
-@app.route("/")
-def index():
-    # print("請求方法", request.method)
-    # print("通訊協定", request.scheme)
-    # print("主機名稱", request.host)
-    # print("路徑", request.path)
-    # print("完整的網址", request.url)
-    # print("瀏覽器和作業系統", request.headers.get("user-agent"))
-    # print("語言偏好", request.headers.get("accept-language"))
-    # print("引薦網址", request.headers.get("referrer"))
-    
-    # lang= request.headers.get("accept-language")
-    # if lang.startswith("en"):
-    #     return redirect("/en")
-    # else:
-    #     return redirect("/zh")
-    # return render_template("index", name="婷婷")
+@app.route("/", methods=["GET", "POST"])
+def upload_file():
+    upload_successful = False
+    if request.method == "POST":
+        uploaded_file = request.files.get('file')
+        if uploaded_file is not None:
+            # 上傳成功
+            upload_successful = True
+        else:
+            upload_successful = False
 
-    return render_template("index.html")
+    if upload_successful:
+        return render_template("index2.html")
+    else:
+        return render_template("index.html")
 
 # 處理路徑 /page 的對應函式
 @app.route("/page")
@@ -61,26 +51,6 @@ def calculate():
         result += n
     return render_template("result.html", data= str(result))
     
-#建立路徑 /en/ 對應的處理函式
-def index_english():
-    return json.dumps({
-        "status":"ok", 
-        "text":"Hello From Ting Ting, Chang"
-        }) #回傳網站首頁的內容
-
-#建立路徑 /zh/ 對應的處理函式
-@app.route("/zh")
-def index_chinese():
-    return json.dumps({
-        "status":"ok", 
-        "text":"您好，歡迎光臨"
-        }, ensure_ascii=False) #指示不要用ASCII編碼處理中文
-    
-#建立路徑 /data 對應的處理函式
-@app.route("/data")
-#用來回應路徑"/data"的處理函式
-def handledata():
-    return "My data"
 
 # 動態路由: 建立路徑 /user/使用者名稱 對應的函式
 @app.route("/user/<username>")
@@ -111,5 +81,3 @@ def getSum():  #min+(min+1)+(min+2)+...+max
 #啟動網站伺服器，可透過"port"參數指定port
 app.run(host="0.0.0.0", port=3000)
 
-#若要終止程式按"ctrl+c" (每次要執行更新過的程式都要先終止目前的程式)
-#打clear 清除之前的命令

71edbeb650a71995587d8142ba3d800171d6a8d6
Melody Chang <contact.melodychang@gmail.com>
2023-10-22 18:09:48 +0800

    build process pipeline for data analysis

--- python_flask.py
+++ python_flask.py
@@ -1,24 +1,30 @@
 # import Flask, request, render_template
 from flask import Flask, request, render_template
 import os
-import csv
+from DM_digestibility_analysis import (
+    read_csv,
+    raw_data_to_dict,
+    multiply_data,
+    subtract_data,
+    calculate_DM_digestibility,
+)
 
 # bulit application object
-app=Flask(__name__)
+app = Flask(__name__)
 
 
-uploaded_file = None
 filepath = None
+
+
 # 建立路徑 / 對應的處理函式
 # /代表網站首頁
-#使用GET來方法，處理路徑 / 的處理函式
+# 使用GET來方法，處理路徑 / 的處理函式
 @app.route("/", methods=["GET", "POST"])
 def upload_file():
     upload_successful = False
-    global uploaded_file
     global filepath
     if request.method == "POST":
-        uploaded_file = request.files['file']
+        uploaded_file = request.files["file"]
         filepath = os.path.join(app.config["file_uploads"], uploaded_file.filename)
         uploaded_file.save(filepath)
         if uploaded_file is not None:
@@ -32,70 +38,35 @@ def upload_file():
     else:
         return render_template("index.html")
 
-app.config["file_uploads"] = "C:\\Users\\melod\\OneDrive\\data_analysis_project\\file_upload"
+
+app.config[
+    "file_uploads"
+] = "C:\\Users\\melod\\OneDrive\\data_analysis_project\\file_upload"
+
 
 # 處理路徑 /show_data 的對應函式
 @app.route("/show_data")
 def show_data():
-    all_rows = []
-    with open(filepath, encoding='utf-8-sig') as csvfile:
-        raw_data= csv.reader(csvfile)
-        # read raw data in row
-        for row in raw_data:
-            # data is added into list "all_rows"
-            all_rows.append(row)
-    
-    return all_rows
-    
+    file_csv = read_csv(filepath)
+    return render_template("show_data_page.html", data=file_csv)
 
-# 處理路徑 /show 的對應函式
-@app.route("/show")
-def show():
-    name= request.args.get("n", "")  #從query string中取得前端使用者在輸入框輸入的文字
-    return "Hi," +name
 
-# 使用POST方法，處利路徑 /calculate 的對應函式
-@app.route("/calculate", methods=["POST"])
-def calculate():
-    # 接收GET方法的query string
-    # max_number=request.args.get("max", "")
-    
-    #接收POST方法的query string
-    max_number=request.form["max"]
-    max_number=int(max_number)  #max_number為str要轉換成int，再計算
-    # 1+2+...+max_number
-    result=0
-    for n in range(1, max_number+1):
-        result += n
-    return render_template("result.html", data= str(result))
-    
+@app.route("/data_analysis")
+def data_analysis():
+    file_csv = read_csv(filepath)
+    data_dict = raw_data_to_dict(file_csv)
+    data_multiply = multiply_data(
+        data_dict, new_key="Diet_DM", key1="Diet_weight", key2="Diet_DM_percentage"
+    )
+    data_substract = subtract_data(
+        data_dict,
+        new_key="Digesta_weight",
+        key1="Freeze_dry_weight",
+        key2="Tube_weight",
+    )
+    data_result = calculate_DM_digestibility(data_multiply, data_substract)
+    return render_template("show_data_analysis_result.html", data=data_result)
 
-# 動態路由: 建立路徑 /user/使用者名稱 對應的函式
-@app.route("/user/<username>")
-def handleusername(username):
-    if username== "婷婷":
-        return "Hello " +username
-    else:
-        return "Hello "+ username
 
-# 建立路徑 /getSum 對應的處理函式
-# 利用要求字串 (Query String) 提供彈性: getSum?min=最小數字&max=最大數字&...
-# 若要求字串中沒有參數min, max,...，則呈現預設值
-@app.route("/getSum")
-def getSum():  #min+(min+1)+(min+2)+...+max
-    #接收要求字串中的參數資料
-    maxNumber= request.args.get("max", 100)
-    #maxNumber從網址取得時為string的形式，要轉換成數字
-    maxNumber=int(maxNumber)
-    minNumber= request.args.get("min", 1)
-    minNumber=int(minNumber)
-    #以下運算 min+(min+1)+(min+2)+...+max 總和的迴圈邏輯
-    result=0
-    for n in range(minNumber,maxNumber+1):
-        result+=n
-    #把結果回應給前端
-    return "結果："+str(result)
-
-#啟動網站伺服器，可透過"port"參數指定port
+# 啟動網站伺服器，可透過"port"參數指定port
 app.run(host="0.0.0.0", port=3000)
-
